430011a61cae6042a6f43cb4ca29e02aeff96a8a
Mohak634 <68562942+Mohak634@users.noreply.github.com>
2026-01-14 01:10:22 +0530

    Navbar
    navbar 1 to 1, scaled too big, fix scale in next commit

--- sections/Navbar/Navbar.tsx
+++ sections/Navbar/Navbar.tsx
@@ -1,16 +1,25 @@
 import styles from './Navbar.module.css'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
     <header className={styles.navbar}>
       <div className={styles.inner}>
         <div className={styles.logo}>
-          Maven
+          <div className={styles.logoImage}>
+            <Image
+              src="/logo.png"
+              alt="Maven AI Logo"
+              width={64}
+              height={64}
+            />
+          </div>
+          <div className={styles.logoText}>Maven</div>
         </div>
 
         <nav className={styles.nav}>
-          <a href="#process">How it works</a>
-          <a href="#features">Features</a>
+          <a href="#solutions">Solutions</a>
+          <a href="#security">Security</a>
           <a href="#pricing">Pricing</a>
         </nav>
 
